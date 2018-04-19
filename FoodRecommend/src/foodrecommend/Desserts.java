107426cebfc48ef683936785203fc46d2fad9511
hyunzeekim <hyunzeekim01@gmail.com>
2018-04-19 10:55:30 -0400

    add class

---
+++ FoodRecommend/src/foodrecommend/Desserts.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodrecommend;
+
+/**
+ *
+ * @author hyunzeekim
+ */
+public class Desserts extends Food {
+    
+}
