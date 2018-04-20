7977097d09ebc2d3451452a2bd974a18bf590d02
Suyoggg <38468533+Suyoggg@users.noreply.github.com>
2018-04-20 10:32:53 -0400

    Update Appetizers.java

--- FoodRecommend/src/foodrecommend/Appetizers.java
+++ FoodRecommend/src/foodrecommend/Appetizers.java
@@ -9,5 +9,21 @@ package foodrecommend;
  * @author hyunzeekim
  */
 public class Appetizers extends Food {
+    boolean bread;
+    boolean salad;
+    boolean soup;
+   
+    
+    public Appetizers( double p, boolean s, boolean h, int c, String r, boolean b, boolean sal, boolean sou){
+        super(p,s,g,c,r);
+        this.meat = b;
+        this.veg = sal;
+        this.highcarbs = sou;
+                
+    }   
+    
+    public void checkFood(){
+        
+    }
     
 }
