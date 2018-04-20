50ecefff93f2da894c55c75c75f8be65429194db
Suyoggg <38468533+Suyoggg@users.noreply.github.com>
2018-04-20 10:34:03 -0400

    Update Desserts.java

--- FoodRecommend/src/foodrecommend/Desserts.java
+++ FoodRecommend/src/foodrecommend/Desserts.java
@@ -9,5 +9,21 @@ package foodrecommend;
  * @author hyunzeekim
  */
 public class Desserts extends Food {
+    boolean beverage;
+    boolean fruit;
+    boolean cake;
+   
+    
+    public Entree( double p, boolean s, boolean h, int c, String r, boolean b, boolean f, boolean ca){
+        super(p,s,g,c,r);
+        this.beverage = b;
+        this.fruit = f;
+        this.cake = ca;
+                
+    }   
+    
+    public void checkFood(){
+        
+    }
     
 }
