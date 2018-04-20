e038f7eb409e86a20eeeff6608d2163b6468e1bd
Suyoggg <38468533+Suyoggg@users.noreply.github.com>
2018-04-20 10:25:18 -0400

    Update Entree.java

--- FoodRecommend/src/foodrecommend/Entree.java
+++ FoodRecommend/src/foodrecommend/Entree.java
@@ -9,5 +9,23 @@ package foodrecommend;
  * @author hyunzeekim
  */
 public class Entree extends Food {
+    boolean meat;
+    boolean veg;
+    boolean highcarbs;
+   
+    
+    public Entree( double p, boolean s, boolean h, int c, String r, boolean m, boolean v, boolean h){
+        super(p,s,g,c,r);
+        this.meat = m;
+        this.veg = v;
+        this.highcarbs = h;
+                
+    }   
+    
+    public void checkRestrictions( String r, ){
+    }
+    
+    
+    
     
 }
