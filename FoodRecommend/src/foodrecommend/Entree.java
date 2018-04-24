a69531331ffba34e9748744fd229480bcd4f5813
Suyoggg <38468533+Suyoggg@users.noreply.github.com>
2018-04-24 11:03:27 -0400

    Update Entree.java

--- FoodRecommend/src/foodrecommend/Entree.java
+++ FoodRecommend/src/foodrecommend/Entree.java
@@ -4,40 +4,58 @@
  */
 package foodrecommend;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author hyunzeekim
  */
 public class Entree extends Food {
-    boolean meat;
-    boolean seafood;
-    boolean noodles;
+    Food[] options;
+    double[] ratings;
    
     
-    public Entree( double p, boolean s, boolean h, int c, String r, boolean m, boolean s, boolean n){
-        super(p,s,g,c,r);
-        this.meat = m;
-        this.seafood = s;
-        this.noodles = n;
-                
+     public Entree(String n, double p, String r, int c, String s, String h, String c1, String c2, String c3){
+        super(n,p,r,c,s,h,c1,c2,c3);             
     }   
     
-    public Food checkFood(Food other){
-        super.checkFood;
+    
+    public Food[] checkFood(FileReader f) throws IOException {
+
+        int index = 0;
         
-        if(other.meat == true && this.meat == true){
+        for(int i = 0; i < super.checkFood(f).length; i++){
+            if(super.checkFood(f)[i].condition1.equals("Y") && fr.meat == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+            if(super.checkFood(f)[i].condition2.equals("Y") && fr.seafood == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+            if(super.checkFood(f)[i].condition3.equals("Y") && fr.noodles == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+                
+        }
+        for (int i = 0; i < options.length; i++){
+            ratings[i] = super.calculateRating(options[i]);
         }
         
-        if(other.seafood == true && this.veg == true){
-        }
         
-        if(other.noodles == true && this.highcarbs == true){
-        }
+        
+        return options;
+     
     }
     
-    public int checkRating(){
-        
-    }
+    public double findTwoMax(double[] a){
+            double max = a[0];
+            for(int b = 1; b < a.length; b++){
+                if(a[b] > max){
+                    max = a[b];
+            }
+        }
+        return max;
+        }
     
     
     
