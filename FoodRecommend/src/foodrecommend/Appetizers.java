041edc78edbc369a4154fd5519c24a284052835d
Suyoggg <38468533+Suyoggg@users.noreply.github.com>
2018-04-25 10:36:00 -0400

    Update Appetizers.java

--- FoodRecommend/src/foodrecommend/Appetizers.java
+++ FoodRecommend/src/foodrecommend/Appetizers.java
@@ -4,37 +4,88 @@
  */
 package foodrecommend;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author hyunzeekim
+ 
  */
 public class Appetizers extends Food {
-    boolean bread;
-    boolean salad;
-    boolean soup;
+    Food[] options;
+    double[] ratings;
+    Food[] finalOptions;
+    FoodRecommend fr = new FoodRecommend();
    
-    
-    public Appetizers( double p, boolean s, boolean h, int c, String r, boolean b, boolean sal, boolean sou){
-        super(p,s,g,c,r);
-        this.bread = b;
-        this.salad = sal;
-        this.soup = sou;
-                
+    //Same constructor made for this
+     public Appetizers(String n, double p, String r, int c, String s, String h, String c1, String c2, String c3){
+        super(n,p,r,c,s,h,c1,c2,c3);             
     }   
     
-        public Food checkFood(Food other){
-        super.checkFood;
-        
-        if(other.bread == true && this.bread == true){
+    
+    public Food[] checkFood(FileReader f) throws IOException {
+        int index = 0;
+        //Uses checkFood method from Food class to create a new array based of 3 conditions
+        for(int i = 0; i < super.checkFood(f).length; i++){
+            if(super.checkFood(f)[i].condition1.equals("Y") && fr.bread == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+            if(super.checkFood(f)[i].condition2.equals("Y") && fr.salad == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+            if(super.checkFood(f)[i].condition3.equals("Y") && fr.soup == true){
+                super.checkFood(f)[i] = options[index++];
+            }
+                
+        }
+     
+        //Recalculates the ratings for the new array of Food
+        for (int i = 0; i < options.length; i++){
+            ratings[i] = super.calculateRating(options[i]);
         }
         
-        if(other.salad == true && this.salad == true){
+        //Used to find highest ratings, and returns an array of index values
+        int[] x = getTwo(ratings);
+        
+        //Uses index values to create a new Food array with the two best options
+        for (int i = 0; i < x.length; i++){
+            finalOptions[x[i]] = options[x[i]];         
         }
         
-        if(other.soup == true && this.soup == true){
-        }
-        
-        
+        return finalOptions;
     }
+    
+    public static int[] getTwo(double[] array){
+      int index1 = 0;
+      int index2 = 0;
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] > index1)
+            {
+                index2 = index1;
+                index1 = i;    
+            }
+            else if (array[i] > index2 && array[i] < index1)
+            {
+                index2 = i;
+            }
+        }
+    return new int[] { index1, index2 };
+    }
+    
+  public void printFood(Food f){
+        double percentage = calculateRating(f);
+        
+        System.out.println(name + " " + price + " " + spicy + " "  + hot + " " + 
+                calorie + " " + restrictions + " " + condition1 + " " + condition2 + " " 
+                + condition3 + percentage);
+    }
+    
+
+    
+    
+    
+    
     
 }
