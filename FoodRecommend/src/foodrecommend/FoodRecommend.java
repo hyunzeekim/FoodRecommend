2bcda12eaa0c1706b9f6751d2b1b40f5bb67c138
hyunzeekim <hyunzeekim01@gmail.com>
2018-04-27 09:50:10 -0400

    food recommend

--- FoodRecommend/src/foodrecommend/FoodRecommend.java
+++ FoodRecommend/src/foodrecommend/FoodRecommend.java
@@ -3,6 +3,8 @@
  * and open the template in the editor.
  */
 package foodrecommend;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -21,15 +23,19 @@ public class FoodRecommend {
     int calorie;
     String restrictions;
     
-    boolean bread = false;
-    boolean salad = false;
-    boolean soup = false;
-    boolean meat = false;
-    boolean seafood = false;
-    boolean noodles = false;
-    boolean beverage = false;
-    boolean fruit = false;
-    boolean dessertother = false;
+    String bread;
+    String salad;
+    String soup;
+    String meat;
+    String seafood;
+    String noodles;
+    String beverage;
+    String fruit;
+    String dessertother;
+    String[] inputs = new String[5];
+    String[] app = new String[3];
+    String[] ent = new String[3];
+    String[] des = new String[3];
     
     boolean wantAppetizer = false;
     boolean wantEntree = false;
@@ -38,7 +44,7 @@ public class FoodRecommend {
     Food [] FinalRecommend = new Food[4];
     
 //The constructor that takes in the input from the user to create a FoodRecommend object
-    public void InputData(){
+    public String[] InputData(){
 
         Scanner s = new Scanner(System.in);
         String currentInput;
@@ -49,9 +55,9 @@ public class FoodRecommend {
         while (!currentInput.equalsIgnoreCase("Spicy") && !currentInput.equalsIgnoreCase("Mild") ){
             System.out.println("Please enter again: Spicy or Mild?" );
             currentInput = s.next();
-        }
+        }       
         spice = currentInput;
-        
+        inputs[0] = spice;
         
         System.out.println("Enter your preference: Hot or Cold?");
         currentInput = s.next();
@@ -69,18 +75,28 @@ public class FoodRecommend {
         calorie = s.nextInt();
         
         System.out.println("Enter one of the following dietary restrictions (Meat / Peanut / Gluten / None): ");
+        System.out.println("If you have more than one, please separate the restriction with a semicolon (e.g. Meat;Peanut)");
         currentInput = s.next();
         
         while(!currentInput.equalsIgnoreCase("Meat")&& !currentInput.equalsIgnoreCase("Peanut")&&!currentInput.equalsIgnoreCase("Gluten") &&
-                !currentInput.equalsIgnoreCase("None")){
-            System.out.println("Incorrect format. Please retype your restrictions (Meat / Peanut / Gluten / None):");
+                !currentInput.equalsIgnoreCase("None")&&!currentInput.equalsIgnoreCase("Meat;Peanut")&&!currentInput.equalsIgnoreCase("Meat;Gluten")
+                &&!currentInput.equalsIgnoreCase("Peanut;Gluten")&&!currentInput.equalsIgnoreCase("Meat;Peanut;Gluten")){
+            System.out.println("Incorrect format. Please retype your restrictions in the order (Meat / Peanut / Gluten / None):");
             currentInput = s.nextLine();
         }     
         restrictions = currentInput;   
+        
+        inputs[0] = spice;
+        inputs[1] = hot;
+        inputs[2] = Double.toString(price);
+        inputs[3] = Integer.toString(calorie);
+        inputs[4] = restrictions;
+        
+        return inputs;
     }
     
 //Asking for if theq user want Appetizers/Entree/Dessert or not
-    public void askForType(){
+    public String askForType(){
         String currentInput;
         Scanner s = new Scanner(System.in);
         
@@ -90,102 +106,118 @@ public class FoodRecommend {
         while (!currentInput.equalsIgnoreCase("Appetizers") && !currentInput.equalsIgnoreCase("Entree") &&
                 !currentInput.equalsIgnoreCase("Desserts")){
             System.out.println("Please enter again (Appetizers / Entree / Desserts)");
-            currentInput = s.nextLine();
+            currentInput = s.next();
         }
-        
-        if(currentInput.equalsIgnoreCase("Appetizers")){
-            AppetizersInput();
-        }        
-        
-        else if(currentInput.equalsIgnoreCase("Entree")){
-            EntreeInput();
-        }        
-        
-        else if(currentInput.equalsIgnoreCase("Desserts")){
-            DessertInput();
-        }
-       
+
+        return currentInput;
     }
 
 // The askForType function calls the AppetizersInput and the other two functions to get 
     //the unique parameters of different types of food.
-    public void AppetizersInput(){
+    public String[] AppetizersInput(){
         String currentInput;
         Scanner s = new Scanner(System.in);
         
         System.out.println("You have selected Appetizers; please type in one of the following (Salad / Bread / Soup):");
 
-        currentInput = s.nextLine();
+        currentInput = s.next();
         while(!currentInput.equalsIgnoreCase("Salad")&&!currentInput.equalsIgnoreCase("Bread")&&
                 !currentInput.equalsIgnoreCase("Soup")){
             System.out.println("Incorrect format. Please retype ( Salad / Bread / Soup )");
-            currentInput = s.nextLine();
+            currentInput = s.next();
         }
         
-        if(currentInput.equalsIgnoreCase("Salad")){
-            salad = true;
+        if(currentInput.equalsIgnoreCase("Bread")){
+            app[0] = "Y";
+            app[1] = "N";
+            app[2] = "N";
         }
-        else if(currentInput.equalsIgnoreCase("Bread")){
-            bread = true;
+        else if(currentInput.equalsIgnoreCase("Salad")){
+            app[0] = "N";
+            app[1] = "Y";
+            app[2] = "N";        
         }
         else if(currentInput.equalsIgnoreCase("Soup")){
-            soup = true;
+            app[0] = "N";
+            app[1] = "N";
+            app[2] = "Y";
         }
+        
+        return app;
     }
     
-    public void EntreeInput(){
+    public String[] EntreeInput(){
         String currentInput;
         Scanner s = new Scanner(System.in);
         
         System.out.println("You have selected Entree, please type in one of the following (Meat / Seafood / Noodles):");
 
-        currentInput = s.nextLine();
+        currentInput = s.next();
         while(!currentInput.equalsIgnoreCase("Meat")&&!currentInput.equalsIgnoreCase("Seafood")&&
                 !currentInput.equalsIgnoreCase("Noodles")){
             System.out.println("Incorrect format. Please retype (Meat / Seafood / Noodles):");
-            currentInput = s.nextLine();
+            currentInput = s.next();
         }
         
         if(currentInput.equalsIgnoreCase("Meat")){
-            meat = true;
+            ent[0] = "Y";
+            ent[1] = "N";
+            ent[2] = "N";        
         }
         else if(currentInput.equalsIgnoreCase("Seafood")){
-            seafood = true;
+            ent[0] = "N";
+            ent[1] = "Y";
+            ent[2] = "N";           
         }
         else if(currentInput.equalsIgnoreCase("Noodles")){
-            noodles = true;
+            ent[0] = "N";
+            ent[1] = "N";
+            ent[2] = "Y";           
         }
+        return ent;
     } 
- public void DessertInput(){
+ public String[] DessertInput(){
         String currentInput;
         Scanner s = new Scanner(System.in);
         
         System.out.println("You have selected Desserts, please type in one of the following (Beverage / Fruit / Other):");
 
-        currentInput = s.nextLine();
+        currentInput = s.next();
         while(!currentInput.equalsIgnoreCase("Beverage")&&!currentInput.equalsIgnoreCase("Fruit")&&
                 !currentInput.equalsIgnoreCase("Other")){
             System.out.println("Incorrect format. Please retype (Beverage / Fruit / Other): ");
-            currentInput = s.nextLine();
+            currentInput = s.next();
         }
         
         if(currentInput.equalsIgnoreCase("Beverage")){
-            beverage = true;
+            des[0] = "Y";
+            des[1] = "N";
+            des[2] = "N";           
         }
         else if(currentInput.equalsIgnoreCase("Fruit")){
-            fruit = true;
+            des[0] = "N";
+            des[1] = "Y";
+            des[2] = "N";          
         }
-        
         else if(currentInput.equalsIgnoreCase("Other")){
-            dessertother = true;
+            des[0] = "N";
+            des[1] = "N";
+            des[2] = "Y";          
         }
+        return des;
     }
     
     //Main 
-    public static void main(String[] args) { 
+    public static void main(String[] args) throws IOException { 
         FoodRecommend f = new FoodRecommend();
-        f.InputData();
-        f.askForType();
+        String[] userInput = f.InputData();
+        String desiredMeal = f.askForType();
+
+        if(desiredMeal.equalsIgnoreCase("Appetizers")){
+            String[] app = f.AppetizersInput();
+            Appetizers a = new Appetizers("", Double.parseDouble(userInput[2]), userInput[4], Integer.parseInt(userInput[3]), userInput[0], userInput[1], app[0], app[1], app[2]);
+            a.printAppetizers();
+        }
     }
 
 }
